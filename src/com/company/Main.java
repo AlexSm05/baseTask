bcce3e140f50054faa59b73e1b5cbf9bfecc39c5
AlexSm05 <alexander.smirnov0527@gmail.com>
2021-07-06 14:03:18 +0300

    text for commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        ArrayList<String> list = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String line;
+            while (!(line = reader.readLine()).equals("конец")) {
+                list.add(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("size of array "+list.size());
+        int min=list.get(0).length(),max=list.get(0).length();
+        for (String str:list) {
+            if (max<str.length()){
+                max=str.length();
+            }else {
+                if (min>str.length()){
+                    min=str.length();
+                }
+            }
+            str.length();
+            System.out.println(str);
+        }
+        System.out.println("min length="+min+" max length="+max);
+    }
+}
